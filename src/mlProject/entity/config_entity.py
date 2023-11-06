5e0334d39f9ecd4316157c383a37aeaa1df17b95
MOHAMMAD-UMAIR <umair.mohd97@gmail.com>
2023-11-06 12:30:57 +0000

    Added data transformation

--- src/mlProject/entity/config_entity.py
+++ src/mlProject/entity/config_entity.py
@@ -7,4 +7,3 @@ class DataIngestionConfig:
     root_dir: Path
     source_URL: str
     local_data_file : Path
-    #unzip_dir : Path
--- src/mlProject/utils/common.py
+++ src/mlProject/utils/common.py
@@ -8,6 +8,7 @@ from ensure import ensure_annotations
 from box import ConfigBox
 from pathlib import Path
 from typing import Any
+import pandas as pd
 
 
 
@@ -68,7 +69,7 @@ def save_json(path: Path, data: dict):
 
 
 @ensure_annotations
-def load_json(path: Path) -> ConfigBox:
+def load_json(path: Path) -> dict:
     """load json files data
 
     Args:
@@ -81,7 +82,7 @@ def load_json(path: Path) -> ConfigBox:
         content = json.load(f)
 
     logger.info(f"json file loaded succesfully from: {path}")
-    return ConfigBox(content)
+    return content
 
 
 @ensure_annotations
@@ -126,5 +127,170 @@ def get_size(path: Path) -> str:
     return f"~ {size_in_kb} KB"
 
 
+@ensure_annotations
+def extract_objects(data, object_type, columns):
+    objects = []
+    if isinstance(data, list):
+        for item in data:
+            objects.extend(extract_objects(item, object_type, columns))
+    elif isinstance(data, dict):
+        type_value = data.get("type")
+        if type_value == object_type:
+            objects.append({key: data.get(key) for key in columns})
+        for key, value in data.items():
+            if isinstance(value, (dict, list)):
+                objects.extend(extract_objects(value, object_type, columns))
+    #logger.info(f"Data extracted sucessfuly from json")
+    return objects
+
+@ensure_annotations
+def load_create_df(path:Path):
+    with open(path, 'r') as file:
+        data = json.load(file)
+
+        # Extract objects for 'attack-pattern'
+        object_type1 = 'attack-pattern'
+        columns1 = ['id', 'name', 'x_mitre_is_subtechnique']
+        objects1 = extract_objects(data, object_type1, columns1)
+        df_attack = pd.DataFrame(objects1)
+        print(df_attack)
+        df_attack.columns = ['attack_id', 'attack_name', 'is_subtechnique']
+        logger.info(f"Data frame created for attack types ")
+
+        # Extract objects for 'malware'
+        object_type2 = 'malware'
+        columns2 = ['id', 'name']
+        objects2 = extract_objects(data, object_type2, columns2)
+        df_malware = pd.DataFrame(objects2)
+        logger.info(f"Data frame created for malwares ")
+
+        # Extract objects for 'relationship'
+        object_type3 = 'relationship'
+        columns3 = ['relationship_type', 'source_ref', 'target_ref']
+        objects3 = extract_objects(data, object_type3, columns3)
+        df_relationship = pd.DataFrame(objects3)
+        logger.info(f"Data frame created for relationship types ")
+        
+        # Extract objects for 'relationship'
+        object_type4 = 'intrusion-set'
+        columns4 = ['id', 'name']
+        objects4 = extract_objects(data, object_type4, columns4)
+        df_intrusion = pd.DataFrame(objects4)
+        logger.info(f"Data frame created for intrusion-sets")
+        
+        
+        # Extract objects for 'relationship'
+        object_type5 = 'campaign'
+        columns5 = ['id', 'name']
+        objects5 = extract_objects(data, object_type5, columns5)
+        df_campaign = pd.DataFrame(objects5)
+        logger.info(f"Data frame created for campaign types ")
+        
+        logger.info(f"Data frames creation done ")
+        
+    return df_attack, df_campaign, df_intrusion, df_malware, df_relationship
+    
+@ensure_annotations
+def attack_to_adversery(df_attack,df_relationship,df_intrusion,df_campaign,file):
+    merge_1 = pd.merge(df_attack, df_relationship, left_on='attack_id', right_on='target_ref', how='left')
+    merge_2 = merge_1.merge(df_intrusion, left_on='source_ref', right_on='id', how='left')\
+    .merge(df_campaign, left_on='source_ref', right_on='id', how='left')
+    merge_2.rename(columns={'id_x': 'intrusion_id', 'name_x': 'intrusion_name','id_y':'campaign_id','name_y':'campaign_name'}, inplace=True)
+    merge_2.drop(columns=['source_ref','target_ref'])
+    
+    logger.info(f"Table operations performed ")
+    
+    def group_rows(group):
+        attack_id = group['attack_id'].iloc[0]
+        attack_name = group['attack_name'].iloc[0]
+        is_subtechnique = group['is_subtechnique'].iloc[0]
+
+        adversary_details = []
+
+        for _, row in group.iterrows():
+            if not pd.isna(row['intrusion_id']):
+                adversary_details.append({
+                    'intrusion_id': row['intrusion_id'],
+                    'intrusion_name': row['intrusion_name']
+                })
+
+            if not pd.isna(row['campaign_id']):
+                adversary_details.append({
+                    'campaign_id': row['campaign_id'],
+                    'campaign_name': row['campaign_name']
+                })
+
+        return adversary_details
+    result_js = merge_2.groupby(['attack_id', 'attack_name', 'is_subtechnique']).apply(group_rows).reset_index()#.to_dict()
+    result_js.rename(columns={0:'adversary_details'}, inplace=True)
+    #result_js = result_js[~result_js['adversary_details'].apply(lambda x: x == [])]
+
+
+    result_dict = result_js.to_dict(orient='records')
+    logger.info(f"Result dict made ")
+    #print(result_dict)
+    #json_str = json.dumps({"objects": result_dict})
+    #json_str = json.dumps(result_dict, indent = 4)
+    #output_path = os.path.join(output_directory, output_file)
+    #with open(output_path, 'w') as file:
+    #json.dump(data, file)
+    if not os.path.exists(file):
+        #os.makedirs(file)
+        with open(file, 'w') as f:
+            json.dump(result_dict,f,indent=4)
+        #f.write(json_str)
+    
+    #json_str = json_str.replace('\n', '')
+
+    #print(type(json_str))
+    #print(json_str)
+    #
+
+
+
+
+# with open('tejas.json', 'w') as f:
+#     f.write(json_str)
+    
+
+
+# @ensure_annotations
+# def group_rows(group):
+#     attack_id = group['attack_id'].iloc[0]
+#     attack_name = group['attack_name'].iloc[0]
+#     is_subtechnique = group['is_subtechnique'].iloc[0]
+
+#     adversary_details = []
+
+#     for _, row in group.iterrows():
+#         if not pd.isna(row['intrusion_id']):
+#             adversary_details.append({
+#                 'intrusion_id': row['intrusion_id'],
+#                 'intrusion_name': row['intrusion_name']
+#             })
+
+#         if not pd.isna(row['campaign_id']):
+#             adversary_details.append({
+#                 'campaign_id': row['campaign_id'],
+#                 'campaign_name': row['campaign_name']
+#             })
+
+#     return adversary_details
+
+# @ensure_annotations
+
+
+# @ensure_annotations
+# def create_data_frames(storage_dict):
+#     data_frames = {}
+#     for type_value, objects in storage_dict.items():
+#         data_frames[type_value] = pd.DataFrame(objects)
+#         logger.info(f"Data frames sucessfully created")
+#     return data_frames
+
+# def save_data_frames(data_frames):
+#     for type_value, df in data_frames.items():
+#         df.to_csv(f"{type_value}.csv", index=False)
+#         logger.info(f"Data frames sucessfully saved in csv")
 
 
